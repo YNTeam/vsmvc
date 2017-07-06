9827161b922fa30c7d1cff91b81f87a05a38e335
pengyour <hansyou@vip.qq.com>
2017-07-06 15:57:28 +0800

    添加项目文件。

---
+++ WinFormsApp/LoginForm.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.summit = new System.Windows.Forms.Button();
+            this.rememberPwd = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.pageSetupDialog2 = new System.Windows.Forms.PageSetupDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboDept = new System.Windows.Forms.ComboBox();
+            this.exit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // summit
+            // 
+            this.summit.Location = new System.Drawing.Point(251, 182);
+            this.summit.Name = "summit";
+            this.summit.Size = new System.Drawing.Size(75, 23);
+            this.summit.TabIndex = 0;
+            this.summit.Text = "登录";
+            this.summit.UseVisualStyleBackColor = true;
+            this.summit.Click += new System.EventHandler(this.summit_Click);
+            // 
+            // rememberPwd
+            // 
+            this.rememberPwd.AutoSize = true;
+            this.rememberPwd.Location = new System.Drawing.Point(251, 153);
+            this.rememberPwd.Name = "rememberPwd";
+            this.rememberPwd.Size = new System.Drawing.Size(72, 16);
+            this.rememberPwd.TabIndex = 1;
+            this.rememberPwd.Text = "记住密码";
+            this.rememberPwd.UseVisualStyleBackColor = true;
+            this.rememberPwd.CheckedChanged += new System.EventHandler(this.记住密码_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "用户名";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(288, 55);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(133, 21);
+            this.txtName.TabIndex = 3;
+            this.txtName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(288, 88);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(133, 21);
+            this.txtPwd.TabIndex = 4;
+            this.txtPwd.TextChanged += new System.EventHandler(this.txtPwd_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(253, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "密码";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(253, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "部门";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // comboDept
+            // 
+            this.comboDept.FormattingEnabled = true;
+            this.comboDept.Items.AddRange(new object[] {
+            "研发部",
+            "财务部",
+            "实施部"});
+            this.comboDept.Location = new System.Drawing.Point(288, 120);
+            this.comboDept.Name = "comboDept";
+            this.comboDept.Size = new System.Drawing.Size(133, 20);
+            this.comboDept.TabIndex = 8;
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(346, 182);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(75, 23);
+            this.exit.TabIndex = 0;
+            this.exit.Text = "退出";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 156);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("华文行楷", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label4.Location = new System.Drawing.Point(21, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(413, 37);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ERP--高端人才管理系统";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 217);
+            this.ControlBox = false;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboDept);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rememberPwd);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.summit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button summit;
+        private System.Windows.Forms.CheckBox rememberPwd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboDept;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WinFormsApp/LoginForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class LoginForm : Form
+    {
+        string MyConnString = "Server=localhost;Database=sampledb;uid=root;Pwd=123456";
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void 记住密码_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void summit_Click(object sender, EventArgs e)
+        {
+            string name = txtName.Text;
+            string pwd = txtPwd.Text;
+            MySqlConnection conn = new MySqlConnection(MyConnString);
+            MySqlCommand cmd;
+            conn.Open();
+            try
+            {
+                cmd = conn.CreateCommand();
+                cmd.CommandText = "select * from t_user where user_name =@userName and password =@pwd ";
+                cmd.Parameters.AddWithValue("@userName",name);
+                cmd.Parameters.AddWithValue("@pwd", pwd);
+                MySqlDataReader user = cmd.ExecuteReader();
+                if (user.HasRows)
+                {
+                    Console.WriteLine("登录成功！");
+                }
+                else
+                {
+                    Console.WriteLine("用户名或密码错误，登录失败！");
+                }
+            }
+            catch {
+
+            }
+        }
+
+        private void txtPwd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
